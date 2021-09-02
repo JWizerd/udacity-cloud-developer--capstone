9913e32ff21effcd311d02b0b846eab4e7e589eb
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2021-09-02 13:58:16 -0600

    setting up base configuration, establishing db connection, installing typeorm, installing nest config service

---
+++ backend/src/config/database.config.ts
@@ -0,0 +1,13 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm/dist/interfaces/typeorm-options.interface';
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: process.env.DATABASE_HOST || '127.0.0.1',
+  port: 3306,
+  username: process.env.DATABASE_USER || 'root',
+  password: process.env.DATABASE_PASS || 'root',
+  database: process.env.DATABASE_DB || 'udacity_uassigned_test',
+  synchronize: process.env.NODE_ENV === "development",
+  bigNumberStrings: false,
+  cache: true,
+};

eb68dab2eb3ce999562bb8f8d2c93b5411b3828e
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2021-09-07 15:41:46 -0600

    removed unnecesary modules, fixing lint errors

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,15 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { databaseConfig } from './config/database.config';
 import { AuthModule } from './auth/auth.module';
-import { JwksClient } from './jwks-client';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true, }),
+    ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot(databaseConfig),
-    AuthModule
+    AuthModule,
   ],
   controllers: [AppController],
-  providers: [AppService, JwksClient],
+  providers: [AppService],
 })
 export class AppModule {}

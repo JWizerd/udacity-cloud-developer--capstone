4159f12000dbd52940b5aca81e38f1ca041e6492
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2021-09-07 15:43:58 -0600

    making auth guard async

--- backend/src/auth/auth.guard.ts
+++ backend/src/auth/auth.guard.ts
@@ -6,21 +6,13 @@ import { Request } from 'express';
 @Injectable()
 export class AuthGuard implements CanActivate {
   constructor(private readonly authService: AuthService) {}
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
     try {
       const request: Request = context.switchToHttp().getRequest();
       const authHeader = request.header('Authorization');
-
-      if (authHeader && authHeader.includes('Bearer')) {
-        return false;
-      }
-
-      this.authService.verifyToken(authHeader.split(' ')[1]);
+      await this.authService.verifyToken(authHeader);
       return true;
     } catch (error) {
-      console.error(error);
       return false;
     }
   }

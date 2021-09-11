77e3fb85d24270aee1d275d18483132baa608017
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2021-09-11 09:48:10 -0600

    adding security, cluster mode for production, global validation pipe and validation libraries

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,30 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+import * as throng from 'throng';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 8081);
+  app.enableCors({ origin: '*' });
+  app.use(helmet());
+  app.useGlobalPipes(new ValidationPipe());
+  app.use(
+    rateLimit({
+      windowMs: 5 * 60 * 1000, // 15 minutes
+      max: 500, // limit each IP to 100 requests per windowMs
+    }),
+  );
+
+  await app.listen(process.env.PORT || 8080);
 }
-bootstrap();
+
+if (process.env.NODE_ENV === 'production') {
+  throng((id) => {
+    console.log(`Started worker ${id}`);
+    bootstrap();
+  });
+} else {
+  bootstrap();
+}

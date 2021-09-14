f30145d0ba3fd9cdcfa176c1af44f42047a7a647
Jeremiah Wodke <jeremiah.wodke@gmail.com>
2021-09-14 16:42:43 -0600

    adding service markets service, and markets service spec

---
+++ backend/src/markets/markets.service.ts
@@ -0,0 +1,58 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersService } from 'src/users/users.service';
+import { TenantedService } from 'src/utils/typeorm/base-tenanted.service';
+import { Repository } from 'typeorm';
+import { CreateMarketDTO } from './dtos/create-market-dto.interface';
+import { UpdateMarketDTO } from './dtos/update-market-dto.interface';
+
+import { Market } from './market.entity';
+
+@Injectable()
+export class MarketsService extends TenantedService<Market> {
+  constructor(
+    @InjectRepository(Market) protected readonly marketRepo: Repository<Market>,
+    private readonly usersService: UsersService,
+  ) {
+    super(marketRepo);
+  }
+
+  async create(userUuid: string, market: CreateMarketDTO): Promise<Market> {
+    const marketEntity = this.marketRepo.create(market);
+    marketEntity.user = await this.usersService.findOne(userUuid);
+    return this.marketRepo.save(market);
+  }
+
+  async findOne(marketId: number): Promise<Market> {
+    return this.usersService.findOne(marketId);
+  }
+
+  async remove(userId: string, marketId: number) {
+    const market = await this.marketRepo.findOne({
+      where: {
+        id: marketId,
+        user: userId,
+      },
+    });
+
+    if (market) {
+      this.marketRepo.remove(market);
+    }
+  }
+
+  async update(
+    userId: string,
+    marketId: number,
+    updateMarketDTO: UpdateMarketDTO,
+  ) {
+    const market = await this.marketRepo.findOne({
+      relations: ['user'],
+    });
+
+    if (market.user.userUuid === userId) {
+      const updatedEntity = this.marketRepo.merge(market, updateMarketDTO);
+      await this.marketRepo.update(marketId, updatedEntity);
+      return updatedEntity;
+    }
+  }
+}
